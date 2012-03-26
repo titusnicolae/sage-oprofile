5522382305c692bf3f03bbe6c795a2afe67bad69
root <root@ty-desktop.(none)>
2012-03-26 21:00:00 +0300

    for loop 10 iterations

--- bench.py
+++ bench.py
@@ -1,17 +1,21 @@
 import os
 from sage.all_cmdline import reset
 from sys import stdout
-from os.path import splitext 
+from os.path import splitext,split 
+from glob import glob
 
-filename="r01_1.py"
-name,ending=splitext(filename)
-f=open(filename, "r")
-os.system("opcontrol --stop")
-os.system("opcontrol --reset")
-os.system("opcontrol --start")
-s=f.read()
-s="from sage.all_cmdline import reset\nreset()\n"+s
-execfile(filename)
-os.system("opcontrol --stop")
-os.system("opreport 1>"+name+".log 2>"+name+".err")
-#os.system("opreport -l 1>"+name+".log2 2>"+name+".err2")
+logdir="log/"
+for filename in glob("py/*.py"):
+	#filename="py/r01_1.py"
+	name,ending=splitext(split(filename)[1])
+	f=open(filename, "r")
+	#os.system("opcontrol --stop")
+	os.system("opcontrol --reset")
+	os.system("opcontrol --start")
+	print filename
+	for i in range(1,10):
+		execfile(filename)
+	os.system("opcontrol --stop")
+	os.system("opreport 1>"+logdir+name+".log 2>"+logdir+name+".err")
+	#os.system("opreport -l 1>"+name+".log2 2>"+name+".err2")
+os.system('grep -r "pynac" log/* | printf "%-15s%-10s%-10s%s\n" `sed "s/\s\{1,\}/ /g"` >statistics10')
